89aad29d1551df3b0e8c19f0a2dec295dfb8d963
denisdbell <denisdbell@gmail.com>
2018-02-21 22:04:17 -0500

    Add additional varibales to constructor

--- lib/start_back_off.rb
+++ lib/start_back_off.rb
@@ -2,7 +2,5 @@ $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 
 require "exponential_backoff.rb"
 
-
-
-ExponentialBackOff.new().start(1,2);
+ExponentialBackOff.new("https://httpbin.org/delay/3",4,1,2).start();
 
