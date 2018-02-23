70de10ad5751edbeb21debce7bd470576dd02ec9
denisdbell <denisdbell@gmail.com>
2018-02-22 20:14:52 -0500

    Include parameters url, max_retries, initial_delay  and delay_multiplier

--- lib/start_back_off.rb
+++ lib/start_back_off.rb
@@ -2,5 +2,9 @@ $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 
 require "exponential_backoff.rb"
 
-ExponentialBackOff.new("https://httpbin.org/delay/3",4,1,2).start();
+url = (!ARGV[0].nil? && !ARGV[0].empty?) ? ARGV[0] : "https://httpbin.org/delay/3" 
+max_retries = (!ARGV[1].nil? && !ARGV[1].empty?)  ? ARGV[1].to_i : 3
+initial_delay = (!ARGV[2].nil? && !ARGV[2].empty?) ? ARGV[2].to_i : 1
+delay_multiplier = (!ARGV[3].nil? && !ARGV[3].empty?) ? ARGV[3].to_i : 2
 
+ExponentialBackOff.new(url,max_retries,initial_delay,delay_multiplier).start();
